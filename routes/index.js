f73bd25a5d4cda39fe54df8cc1762afd834ed8c1
Md. Asadul Islam <asad.cse.ru.15@gmail.com>
2017-10-16 23:46:26 +0600

    get method is working well

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var todo = require('../models/todos');
+
+mongoose.Promise = require('bluebird');
 
 router.get('/', function (req, res, next) {
-	res.render('index', { title: 'Todos Api', name: 'This is just simple'});
+	mongoose.connect('mongodb://localhost/mydb', function (err) {
+        if (err) {
+            console.log('can not connect database');
+            throw err;
+        }
+        console.log('database connected');
+    });
+
+    todo.find(function (err, rows) {
+    	if(err){
+    		res.send('database error', {title: 'error'});
+    	}
+    	else {
+            res.render('index', {title: 'Todos Api', name: 'All todos data', rows: rows});
+        }
+    });
+
+	mongoose.disconnect(function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Connection Closed');
+        }
+    });
 });
 
 module.exports = router;
