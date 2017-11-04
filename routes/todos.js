411dea53b846373d7437ad0261fcb3b8d1d308b7
Md. Asadul Islam <asad.cse.ru.15@gmail.com>
2017-11-04 21:56:59 +0600

    Put method is working well :)

--- routes/todos.js
+++ routes/todos.js
@@ -5,7 +5,12 @@ var todo = require('../models/todos');
 
 mongoose.Promise = require('bluebird');
 
+/**
+ * Haldling get('/todos') request
+ */
+
 router.get('/todos', function(req, res, next) {
+	//connecting database
 	mongoose.connect('mongodb://localhost/mydb', function(err) {
 		if (err) {
 			console.log('can not connect database');
@@ -14,6 +19,7 @@ router.get('/todos', function(req, res, next) {
 		console.log('database connected');
 	});
 
+	// finding all todo information and send to webpage
 	todo.find(function(err, rows) {
 		if (err) {
 			res.send('database error', {
@@ -37,8 +43,12 @@ router.get('/todos', function(req, res, next) {
 	});
 });
 
-router.post('/todos', function(req, res, next) {
+/**
+ * Haldling post('/todos') request
+ */
 
+router.post('/todos', function(req, res, next) {
+	//checking for missing input
 	if (!req.body.id || !req.body.name || !req.body.completed || !req.body.updated_at) {
 		res.send('Sorry, fill in the form correctly.');
 		//res.redirect('/todos');
@@ -49,13 +59,15 @@ router.post('/todos', function(req, res, next) {
 				throw err;
 			}
 			console.log('database connected');
-		})
+		});
+		//create new object for database schema
 		var newTodo = new todo({
 			id: req.body.id,
 			name: req.body.name,
 			completed: req.body.completed,
 			updated_at: req.body.updated_at
 		});
+		//add record to database
 		newTodo.save(function(err, result) {
 			if (err) {
 				res.send('database error');
@@ -71,6 +83,35 @@ router.post('/todos', function(req, res, next) {
 			}
 		});
 	}
+});
+
+router.put('/todos/:id', function (req, res, next) {
+    mongoose.connect('mongodb://localhost/mydb', function(err) {
+        if (err) {
+            console.log('can not connect database');
+            throw err;
+        }
+        console.log('database connected');
+    });
+
+    todo.findById(req.params.id, function (err, todo) {
+        if (err){
+            res.send('No data found');
+        } else {
+            if (!req.body.name || !req.body.completed || !req.body.updated_at) {
+                res.send('Sorry, fill in the form correctly.');
+                //res.redirect('/todos');
+            } else {
+                todo.update({"id": req.params.id}, {"name": req.body.name, "completed": req.body.completed, "updated_at": req.body.updated_at}, function(err, result) {
+                    if (err) {
+                        res.send('database error');
+                    } else {
+                        res.send('Updated ' + result);
+                    }
+                });
+            }
+        }
+    });
 });
 
 module.exports = router;
